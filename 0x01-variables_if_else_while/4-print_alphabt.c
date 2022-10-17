1fb87d5ead4ecd6a79dd05747a431d466a262f78
worthyEmerald <emerald4u95@gmail.com>
2022-10-17 13:11:12 -0700

    4-prints

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Always)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	if (i == 'e' || i == 'q')
+		continue;
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
