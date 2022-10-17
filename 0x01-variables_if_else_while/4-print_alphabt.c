14948c52596293c3c15865ae7ff0dd841b676a97
worthyEmerald <emerald4u95@gmail.com>
2022-10-17 13:23:04 -0700

    Prints

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,10 @@ int main(void)
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-	if (i == 'e' || i == 'q')
-		continue;
 	{
 		putchar(i);
+	if (i == 'e' || i == 'q')
+		continue;
 	}
 	putchar('\n');
 	return (0);
