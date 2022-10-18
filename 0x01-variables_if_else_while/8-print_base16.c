36e27264790a8a8f2e431d53745467874992cf48
worthyEmerald <emerald4u95@gmail.com>
2022-10-17 19:10:09 -0700

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	char hexavalues[] = "0123456789abcdef";
+
+	for (i = 0; i <= 16; i++)
+	{
+		putchar(hexavalues[i]);
+	}
+	putchar('\n');
+	return (0);
+}
