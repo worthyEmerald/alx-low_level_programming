7d30e55072347e92abe38abb01f95442b5122e6d
worthyEmerald <emerald4u95@gmail.com>
2022-10-23 21:09:21 +0000

    Nested Loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 -14 10 times
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i, ch;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 0; ch < 16; ch++)
+		{
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
