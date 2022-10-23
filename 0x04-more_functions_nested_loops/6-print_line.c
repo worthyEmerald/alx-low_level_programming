a2906c1165467ce63864d9f3febda41cdc689712
worthyEmerald <emerald4u95@gmail.com>
2022-10-23 21:19:33 +0000

    Nested Loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ *
+ * @n: times straight line is printed
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; n++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
