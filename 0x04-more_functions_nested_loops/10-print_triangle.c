f85ae3bd955707a42bd130bd737611b4e6159430
worthyEmerald <emerald4u95@gmail.com>
2022-10-24 03:56:22 +0000

    Nested Loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle in the terminal
+ * @size: number of triangle to be printed
+ * Return: nothing
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - 1); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
