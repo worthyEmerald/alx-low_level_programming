5568b7d9e9a137ee41382dcc579e39ba551ceeb2
worthyEmerald <emerald4u95@gmail.com>
2022-11-09 22:02:34 +0000

    Arg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,19 +11,18 @@
   */
 int main(int argc, char *argv[])
 {
-        int n1 = 0, n2 = 0;
+	int n1 = 0, n2 = 0;
 
-        if (argc == 3)
-        {
-                n1 = atoi(argv[1]);
-                n2 = atoi(argv[2]);
-                printf("%d\n", n1 * n2);
-        }
-        else
-        {
-                printf("Error\n");
-                return (1);
-        }
-
-        return (0);
+	if (argc == 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,35 +12,31 @@
   */
 int main(int argc, char *argv[])
 {
-        int i;
-        unsigned int k, sum = 0;
-        char *e;
+	int i;
+	unsigned int k, sum = 0;
+	char *e;
 
-        if (argc > 1)
-        {
-                for (i = 1; i < argc; i++)
-                {
-                        e = argv[i];
-
-                        for (k = 0; k < strlen(e); k++)
-                        {
-                                if (e[k] < 48 || e[k] > 57)
-                                {
-                                        printf("Error\n");
-                                        return (1);
-                                }
-                        }
-
-                        sum += atoi(e);
-                        e++;
-                }
-
-                printf("%d\n", sum);
-        }
-        else
-        {
-                printf("0\n");
-        }
-
-        return (0);
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			e = argv[i];
+			for (k = 0; k < strlen(e); k++)
+			{
+				if (e[k] < 48 || e[k] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(e);
+			e++;
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
 }
