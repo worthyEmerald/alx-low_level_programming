84d5b944401454778f1eb32507dad9ea845649e2
worthyEmerald <emerald4u95@gmail.com>
2022-11-10 21:37:31 +0000

    String concatenates

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenates two strings
+ * string_nconcat-concatenates two strings
  * @s1: first string
  * @s2: second string
  * @n: amount of bytes
- * Return pointer to the allocated memory, else value is 98
+ * Return: pointer to the allocated memory, else value is 98
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
